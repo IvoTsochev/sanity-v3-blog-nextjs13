1f47e040e1d4bf9b7fe267e575e9e8f8fbd75241
Ivo Tsochev <ivo.tsochev@live.com>
2023-04-14 01:10:35 +0300

    Symbol issue fix

--- components/NewsletterModal.tsx
+++ components/NewsletterModal.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 
 const NewsletterModal = () => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   const subscribe = async () => {
-    const res = await fetch('/api/subscribe', {
+    const res = await fetch("/api/subscribe", {
       body: JSON.stringify({
-        email: inputRef.current?.value
+        email: inputRef.current?.value,
       }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      method: 'POST'
+      method: "POST",
     });
 
     const { error } = await res.json();
@@ -23,10 +23,10 @@ const NewsletterModal = () => {
     if (error) {
       setMessage(error);
       return;
-    };
+    }
 
-    inputRef.current!.value = '';
-    setMessage('Success! 🔥🔥🚀🚀 You are now subscribed to the newsletter.');
+    inputRef.current!.value = "";
+    setMessage("Success! 🔥🔥🚀🚀 You are now subscribed to the newsletter.");
   };
 
   return (
@@ -36,26 +36,31 @@ const NewsletterModal = () => {
         to my newsletter.
       </h1>
 
-      {
-        !message ? (
-          <>
+      {!message ? (
+        <>
           <p className="mb-6">
-        I won't spam you with emails. I will only send you the best content. 
-      </p>
+            I won't spam you with emails. I will only send you the best content.
+          </p>
 
-      <div className="flex bg-gradient-to-r from-red-500 to-orange-500 p-[3px]">
-        <input
-          type="email" 
-          className="w-full max-w-[350px] p-2 focus:outline-none text-black"
-          placeholder="Enter your email"
-          ref={inputRef}
-          name="email"
-        />
-        <button className="w-full text-center" onClick={subscribe}>Subscribe to newsletter > </button>
-      </div>
-          </>
-        ) : message
-      }
+          <div className="flex bg-gradient-to-r from-red-500 to-orange-500 p-[3px]">
+            <input
+              type="email"
+              className="w-full max-w-[350px] p-2 focus:outline-none text-black"
+              placeholder="Enter your email"
+              ref={inputRef}
+              name="email"
+            />
+            <button
+              className="w-full text-center"
+              onClick={subscribe}
+            >
+              Subscribe to newsletter {">"}{" "}
+            </button>
+          </div>
+        </>
+      ) : (
+        message
+      )}
     </div>
   );
 };
